6a6a515fcfa25336415b8843abdf55ed417a711c
Colin Sprows <colin.sprows@gmail.com>
2023-03-13 18:57:51 -0400

    added some test data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,11 @@
 -- department seed
 INSERT INTO department (name)
+VALUES ("Legal")
 
 -- role seed
 INSERT INTO role (title, salary, department_id)
+VALUES ("Legal Guy", 69420, 1)
 
 -- employee seed
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Legal", "Guy", 1, 1)
