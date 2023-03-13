ad0fdd28e856f2826b2bb7178105c0b351fceb21
Colin Sprows <colin.sprows@gmail.com>
2023-03-13 18:54:16 -0400

    set up seeds

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+-- department seed
+INSERT INTO department (name)
+
+-- role seed
+INSERT INTO role (title, salary, department_id)
+
+-- employee seed
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
