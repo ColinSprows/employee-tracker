da166857dbc8477c461f463a853f5b125b37a090
Colin Sprows <colin.sprows@gmail.com>
2023-03-14 15:28:12 -0400

    finally got add functions working

--- index.js
+++ index.js
@@ -7,11 +7,16 @@ const consoleTable = require("console.table");
 const db = mysql.createConnection({
     user: "root",
     password: "6666",
-    database: "employee_info_db",
+    database: "employee_db",
     host: "localhost",
 },
 console.log("db connected")
 )
+
+db.connect(function(err) {
+    if (err) throw err;
+    initPrompt();
+})
 
 // DATA
 
@@ -48,13 +53,109 @@ function viewAllEmployees() {
     });
 }
 
-function addADepartment() {}
+function addADepartment() {
+    inquirer
+        .prompt({
+            type: "input",
+            message: "Please enter new department name",
+            name: "departmentName"
+        })
+        .then(function(answer){
+            db.query(`INSERT INTO department (name) VALUES (?)`, [answer.departmentName]), 
+            (err, response) => {
+                if (err) throw err;
+                console.table(response) 
+            }
+            initPrompt();
+        })
+}
 
-function addARole() {}
+function addARole() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "Please select a role name",
+                name: "roleName"
+            },
+            {
+                type: "input",
+                message: "Please enter the salary for this role",
+                name: "salary"
+            },
+            {
+                type: "input",
+                message: "Please enter the department that this role is in",
+                name: "departmentName"
+            }
+        ])
+        .then(function(answer) {
+            db.query(`INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`, [answer.roleName, answer.salary, answer.departmentName]), 
+            (err, response) => {
+                if (err) throw err;
+                console.table(response)
+            }
+            initPrompt();
+        })
+}
 
-function addAmEmployee() {}
+function addAnEmployee() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "Enter first name",
+                name: "firstName"
+            },
+            {
+                type: "input",
+                message: "Enter last name",
+                name: "lastName"
+            },
+            {
+                type: "input",
+                message: "Enter role",
+                name: "role"
+            },
+            {
+                type: "input",
+                message: "Enter manager id",
+                name: "manager"
+            }
+        ])
+        .then(function(answer) {
+            db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`, [answer.firstName, answer.lastName, answer.role, answer.manager]), 
+            (err, response) => {
+                if (err) throw err;
+                console.table(response)
+            }
+            initPrompt();
+        })
+}
 
-function updatAnEmployeeRole() {}
+function updatAnEmployeeRole() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "Enter an employee to update",
+                name: "employee"
+            },
+            {
+                type: "input",
+                message: "Enter employees new role",
+                name: "newRole"
+            },
+        ])
+        .then(function(answer) {
+            db.query(`UPDATE employee SET role_id=? WHERE first_name= ?`, [answer.employee, answer.newRole]), 
+            (err, response) => {
+                if (err) throw err;
+                console.table(response)
+            }
+            initPrompt();
+        })
+}
 
 // APP / PORT
 const PORT = process.env.PORT || 3001;
@@ -83,7 +184,7 @@ function initPrompt() {
         .then(function(answer) {
             console.log("You chose: " + answer.init);
             // add functions for each choice above and initialize on select
-            switch (answer.option) {
+            switch (answer.init) {
                 case "View all departments":
                     viewAllDepartments();
                     break;
@@ -100,7 +201,7 @@ function initPrompt() {
                     addARole();
                     break;
                 case "Add an employee":
-                    addAmEmployee();
+                    addAnEmployee();
                     break;
                 case "Update an employee role":
                     updatAnEmployeeRole();
@@ -113,4 +214,3 @@ function initPrompt() {
 }
 
 // INIT
-initPrompt();
