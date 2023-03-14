8d7cf5ab889fa063f99fbb6649102dc06e625208
Colin Sprows <colin.sprows@gmail.com>
2023-03-14 11:50:55 -0400

    got the prompt data in and setup empty functions

--- server.js
+++ server.js
@@ -3,9 +3,6 @@ const mysql = require('mysql2')
 const inquirer = require("inquirer")
 const consoleTable = require("console.table");
 
-
-// DATA
-
 // DB CONNECTION
 const db = mysql.createConnection({
     user: "root",
@@ -15,6 +12,31 @@ const db = mysql.createConnection({
 },
 console.log("db connected")
 )
+
+// DATA
+
+// FUNCTIONS
+    //functions for inquirer
+
+function quit() {
+    db.end();
+    process.exit();
+}
+
+
+function viewAllDepartments() {}
+
+function viewAllRoles() {}
+
+function viewAllEmployees() {}
+
+function addADepartment() {}
+
+function addARole() {}
+
+function addAmEmployee() {}
+
+function updatAnEmployeeRole() {}
 
 // APP / PORT
 const PORT = process.env.PORT || 3001;
@@ -31,14 +53,46 @@ function initPrompt() {
             type: "list",
             message: "What would you like to do?",
             choices: [
-                "a",
-                "b",
-                "c",
+                "View all departments",
+                "View all roles",
+                "View all employees",
+                "Add a department",
+                "Add a role",
+                "Add an employee",
+                "Update an employee role"
             ]
         })
         .then(function(answer) {
             console.log("You chose: " + answer.init);
+            // add functions for each choice above and initialize on select
+            switch (answer.option) {
+                case "View all departments":
+                    viewAllDepartments();
+                    break;
+                case "View all roles":
+                    viewAllRoles();
+                    break;
+                case "View all employees":
+                    viewAllEmployees();
+                    break;
+                case "Add a department":
+                    addADepartment();
+                    break;
+                case "Add a role":
+                    addARole();
+                    break;
+                case "Add an employee":
+                    addAmEmployee();
+                    break;
+                case "Update an employee role":
+                    updatAnEmployeeRole();
+                    break;
+                default:
+                    quit();
+            }
+            // call functions above in FUNCTIONS
         })
 }
 
+// INIT
 initPrompt();
